a9627e9aa36099d5b6da988dbc6274049748fe36
Hartmut Kaiser <hartmut.kaiser@gmail.com>
2023-07-04 21:08:07 -0500

    Finish code

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-# CSC3380 Object Oriented Programming using C++ (Fall 2023) - Code for Lecture 5
+# CSC3380 Object Oriented Programming using C++ (Fall 2023) - Code for Lecture 6
 
-See here for more information about [Lecture 5][lecture5]
+See here for more information about [Lecture 6][lecture6]
 
-[lecture5]: https://hkaiserteaching.github.io/fall2023/csc3380/course/lecture5.html
+[lecture6]: https://hkaiserteaching.github.io/fall2023/csc3380/course/lecture6.html
--- code/calculator.cpp
+++ code/calculator.cpp
@@ -3,10 +3,104 @@
 #include <stdexcept>
 #include <string>
 
-// Token stuff (explained in the next lecture)
+// Token stuff
+struct token
+{
+    char kind;       // what kind of token
+    double value;    // for numbers: a value
+
+    // constructors
+    token(char ch)
+      : kind(ch)
+      , value(0)
+    {
+    }
+    token(char ch, double val)
+      : kind(ch)
+      , value(val)
+    {
+    }
+};
+
+class token_stream
+{
+    // representation: not directly accessible to users:
+    bool full;       // is there a token in the buffer?
+    token buffer;    // here is where we keep a Token put back using
+                     // putback()
+public:
+    // user interface:
+    token get();            // get a token
+    void putback(token);    // put a token back into the token_stream
+
+    // constructor: make a token_stream, the buffer starts empty
+    token_stream()
+      : full(false)
+      , buffer(0)
+    {
+    }
+};
+
+void token_stream::putback(token t)
+{
+    if (full)
+        throw std::runtime_error("putback() into a full buffer");
+    buffer = t;
+    full = true;
+}
+
+token token_stream::get()    // read a token from the token_stream
+{
+    if (full)
+    {
+        full = false;
+        return buffer;
+    }    // check if we already have a Token ready
+
+    char ch;
+    std::cin >> ch;    // note that >> skips whitespace (space, newline, tab, etc.)
+
+    switch (ch)
+    {
+    case '(': case ')': case ';': case 'q':
+    case '+': case '-': case '*': case '/':
+        return token(ch);    // let each character represent itself
+    case '.': case '0': case '1': case '2': case '3':
+    case '4': case '5': case '6': case '7': case '8':
+    case '9':
+    {
+        std::cin.putback(ch);    // put digit back into the input stream
+        double val;
+        std::cin >> val;                // read a floating-point number
+        return token('8', val);    // let ‘8’ represent “a number”
+    }
+    default:
+        throw std::runtime_error("Bad token");
+    }
+}
 
 // declaration so that primary() can call expression()
 double expression();
+
+double primary()    // Number or ‘(‘ Expression ‘)’
+{
+    token t = get_token();
+    switch (t.kind)
+    {
+    case '(':    // handle ‘(’expression ‘)’
+    {
+        double d = expression();
+        t = get_token();
+        if (t.kind != ')')
+            throw std::runtime_error("')' expected");
+        return d;
+    }
+    case '8':    // we use ‘8’ to represent the “kind” of a number
+        return t.value;    // return the number’s value
+    default:
+        throw std::runtime_error("primary expected");
+    }
+}
 
 // exactly like expression(), but for * and /
 double term()
@@ -31,26 +125,6 @@ double term()
         default:
             return left;    // return the value
         }
-    }
-}
-
-double primary()    // Number or ‘(‘ Expression ‘)’
-{
-    token t = get_token();
-    switch (t.kind)
-    {
-    case '(':    // handle ‘(’expression ‘)’
-    {
-        double d = expression();
-        t = get_token();
-        if (t.kind != ')')
-            throw std::runtime_error("')' expected");
-        return d;
-    }
-    case '8':    // we use ‘8’ to represent the “kind” of a number
-        return t.value;    // return the number’s value
-    default:
-        throw std::runtime_error("primary expected");
     }
 }
 
